e358263b4d83f24f8a2622f39f590e42a588c86a
Arthur Rodrigues Proença <arthur.rodriguesproenca@gmail.com>
2023-06-13 00:47:33 -0300

    chore: tela inicial de busca

---
+++ lib/src/screens/home/home.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class SearchForm extends StatelessWidget {
+  const SearchForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "UAI?",
+      home: SearchFormQuery(),
+    );
+  }
+}
+
+class SearchFormQuery extends StatefulWidget {
+  const SearchFormQuery({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SearchFormQuery();
+}
+
+class _SearchFormQuery extends State<SearchFormQuery> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: SizedBox(
+                width: 300,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 200,
+                        padding: const EdgeInsets.all(40),
+                        child: Image.asset('assets/logo/logo.png'),
+                      ),
+                      TextField(
+                        controller: controller,
+                        decoration: const InputDecoration(
+                            focusColor: Colors.orange,
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            hintText: 'Cadê...'),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.orange)),
+                          onLongPress: () => Navigator.pushNamed(context, '/'),
+                          onPressed: () {
+                            // Navigator.pushNamed(context, '/search',
+                            //     arguments: controller.text);
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                      title: const Text('Busca'),
+                                      content: Text(controller.text),
+                                      actions: [
+                                        TextButton(
+                                            onPressed: () =>
+                                                Navigator.pop(context),
+                                            child: const Text('Fechar'))
+                                      ]);
+                                });
+                          },
+                          child: const Text('Buscar'))
+                    ]))));
+  }
+}
